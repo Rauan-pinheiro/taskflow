9fd0ca0bf316d4827d0aeafea29a5e98cc0046a0
Rauan Pinheiro <rauanpinheiro.dip@gmail.com>
2026-01-31 13:01:19 -0300

    Registrando o model na admin e criando o superuser

--- tarefas/admin.py
+++ tarefas/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(Tarefa)
+class TarefaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'priority' ,'status', 'criation_date')
+    
+    list_filter = ('priority', 'status')
+
+    search_fields = ('title',)
